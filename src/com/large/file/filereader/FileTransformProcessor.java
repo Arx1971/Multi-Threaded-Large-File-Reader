8b878724c7515aecf23cef711fdd9a6397744520
Adnan Rahin <adnan.rahin13@gmail.com>
2021-02-28 02:42:07 -0500

    flie closed

--- src/com/large/file/filereader/FileTransformProcessor.java
+++ src/com/large/file/filereader/FileTransformProcessor.java
@@ -18,7 +18,7 @@ public class FileTransformProcessor {
 
         BufferedReader bufferedReader = null;
         BufferedWriter bufferedWriter = null;
-        
+
         try {
 
             bufferedReader = new BufferedReader(
@@ -39,7 +39,6 @@ public class FileTransformProcessor {
             assert bufferedReader != null;
             bufferedReader.close();
             bufferedWriter.close();
-
         }
 
     }

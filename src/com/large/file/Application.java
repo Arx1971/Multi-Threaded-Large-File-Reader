4c0c15da5a7160e824104a4dd0783e38a7a79989
Adnan Rahin <adnan.rahin13@gmail.com>
2021-02-27 20:41:14 -0500

    File Reader Writing Error

--- src/com/large/file/Application.java
+++ src/com/large/file/Application.java
@@ -1,13 +1,14 @@
 package com.large.file;
 
+import com.large.file.cutomfilewriter.GenerateFile;
+
 public class Application {
 
     public static void main(String[] args) {
-        /*CustomFileWriter generateFile =
-                new CustomFileWriter("c:/Users/rahin/online/customwriter",
-                        "/customFileWriter-2021-02-27-18-28-39.txt"
-                        , 196L);
-        generateFile.largeFileWriter();*/
+        GenerateFile generateFile =
+                new GenerateFile("c:/Users/rahin/online/customwriter/"
+                        , 2000000L, 100);
+
     }
 
 }
--- src/com/large/file/cutomfilewriter/GenerateFile.java
+++ src/com/large/file/cutomfilewriter/GenerateFile.java
@@ -1,26 +1,43 @@
 package com.large.file.cutomfilewriter;
 
-public class GenerateFile implements Runnable{
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class GenerateFile implements Runnable {
 
     private final String filePath;
     private final Long numberOfCharacters;
-    private final String fileName;
+    private final String fileName = new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss'.txt'").format(new Date());;
     private final Integer numberOfThreads;
+    private final CustomFileWriter customFileWriter;
 
-    public GenerateFile(String filePath, String fileName, Long numberOfCharacters, Integer numberOfThreads) {
+    public GenerateFile(String filePath, Long numberOfCharacters, Integer numberOfThreads) {
         this.filePath = filePath;
         this.numberOfCharacters = numberOfCharacters;
         this.numberOfThreads = numberOfThreads;
-        this.fileName = fileName;
+        this.customFileWriter = new CustomFileWriter(this.filePath, this.fileName, this.numberOfCharacters);
         generateFile();
     }
 
     public void generateFile() {
-
+        Thread[] threads = new Thread[numberOfThreads];
+        for (int i = 0; i < numberOfThreads; i++) {
+            threads[i] = new Thread(this);
+            threads[i].start();
+        }
+        for (int i = 0; i < numberOfThreads; i++) {
+            try {
+                threads[i].join();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     @Override
     public void run() {
-
+        synchronized (customFileWriter) {
+            customFileWriter.largeFileWriter();
+        }
     }
 }
